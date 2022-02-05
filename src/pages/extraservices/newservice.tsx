caed73f1a832afb0d877106c7c900a5e5d05f455
Bruno Carvalho <brunocarvalho15032001@outlook.pt>
2022-02-05 11:48:36 +0000

    newservice

--- src/pages/extraservices/newservice.tsx
+++ src/pages/extraservices/newservice.tsx
@@ -28,6 +28,7 @@ import {
   IonCheckbox,
   CheckboxCustomEvent,
   CheckboxChangeEventDetail,
+  IonInput,
 } from "@ionic/react";
 import {
   pin,
@@ -39,6 +40,8 @@ import {
   createOutline,
   arrowForward,
   addCircle,
+  addCircleOutline,
+  addOutline,
 } from "ionicons/icons";
 import { useHistory } from "react-router-dom";
 import "./newservice.css";
@@ -48,6 +51,29 @@ import { ticketOutline, today } from "ionicons/icons";
 import { decodeToken } from "react-jwt";
 
 const NewService: React.FC = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const history = useHistory();
+  const addnewservices = () => {
+    if (name == "" || price == "") {
+      alert("Invalid product.");
+    } else {
+      axios
+        .post("https://remis.jbr-projects.pt/db/index.php?f=newservice", {
+          name: name,
+          price: price,
+        })
+        .then((response) => {
+          if (response.data == 0) {
+            console.log("new product not registered");
+            alert("New product not registered.");
+          } else {
+            console.log(response.data);
+            alert("New product registered.");
+          }
+        });
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -58,7 +84,37 @@ const NewService: React.FC = () => {
           <IonTitle>New Services</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent></IonContent>
+      <IonContent>
+        <div className="service">
+            <h1 className="create">Create a new service</h1>
+          <div className="firstnameRegisto">
+            <input
+              className="registoplaceholder"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              placeholder="Insert the name of new service.."
+              required
+            />
+          </div>
+          <div className="firstnameRegisto">
+            <input
+              className="registoplaceholder"
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value.replace(/[^0-9]/g, ""))}
+              placeholder="Insert the price of new service.."
+              
+            />
+          </div>
+          <div className="totalbuttonregistar">
+            <button onClick={addnewservices} className="botaoService">
+                New Service
+            </button>
+          </div>
+        </div>
+      </IonContent>
     </IonPage>
   );
 };
